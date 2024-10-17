a142bee4cb71611d06ae1a747e5f007037dead91
shay <shay@utah.edu>
2024-10-17 14:35:01 -0600

    em clustering of extreme error points & fitting with sqem

--- test_ps.py
+++ test_ps.py
@@ -5,6 +5,7 @@ from bary_centric import TetBaryCentricCompute
 from pyqmat import nqmat
 import numpy as np
 from off import write_off
+from sklearn.mixture import GaussianMixture
 
 from sqem import Sqem
 class PSViewer():
@@ -54,12 +55,9 @@ class PSViewer():
         # print(b, tid)
         # print(tbtt.bc)
         # print(tbtt.tids)
+        self.num_spheres = 1
 
 
-
-    def my_function(self):
-        # ... do something important here ...
-        print("executing function")
 
 # Define our callback function, which Polyscope will repeatedly execute while running the UI.
 # We can write any code we want here, but in particular it is an opportunity to create ImGui 
@@ -120,37 +118,84 @@ class PSViewer():
             self.hausdorff = self.mesh.add_scalar_quantity("hausdorff", self.h, enabled = True)
             print("hausdorff computed")
             
-        if (gui.Button("save extreme set")):
-            # saving for greedy optimization
-            write_off(f"{self.fitting_save_folder}/mesh.off", self.V_deform, self.surface_F)
-            np.save(f"{self.fitting_save_folder}/points_set.npy", self.points_set)
-            np.save(f"{self.fitting_save_folder}/faces_set.npy", self.faces_set)
-            self.tbtt.slabmesh.export_ma(f'{self.fitting_save_folder}/slabmesh.ma')
+        # if (gui.Button("save extreme set")):
+        #     # saving for greedy optimization
+        #     write_off(f"{self.fitting_save_folder}/mesh.off", self.V_deform, self.surface_F)
+        #     np.save(f"{self.fitting_save_folder}/points_set.npy", self.points_set)
+        #     np.save(f"{self.fitting_save_folder}/faces_set.npy", self.faces_set)
+        #     self.tbtt.slabmesh.export_ma(f'{self.fitting_save_folder}/slabmesh.ma')
+
         changed, self.threshold = gui.SliderFloat("threshold", self.threshold, v_min = 0.0, v_max = 1.0)
         if (gui.Button("extract maximum points")):
             self.extract_max_points()
             
+
+        changed, self.num_spheres = gui.InputInt("#spheres", self.num_spheres, step = 1, step_fast = 10)
+        if (gui.Button("em cluster")):
+            self.em_cluster()
         if (gui.Button("add sphere")):
             self.add_sphere()
+
+    def em_cluster(self):
+        gmm = GaussianMixture(n_components= self.num_spheres, covariance_type='full')
+
+        points = self.V_deform[self.points_set]
+        gmm.fit(points)
+        
+        self.labels = np.zeros_like((self.V_deform.shape[0]), dtype = np.int32)
+
+        labels = gmm.predict(self.V_deform)
+        self.labels = np.where(self.points_set, labels, -1) 
+        print(self.labels.dtype)
+        self.label_quantity = self.mesh.add_scalar_quantity("label", self.labels, enabled = True)
+
+
 
 
     def add_sphere(self): 
         self.N = igl.per_face_normals(self.V_deform, self.surface_F, np.array([0.0, 0.0, 1.0]))
-        sum = Sqem()
-        sum.set_zero()
-        for f in self.faces_set:
-            p = self.V_deform[self.surface_F[f, 0]]
-            n = self.N[f]
-            sf = Sqem(p, n)
-            sum = sum + sf
 
-        pa = np.ones((3)) * -10.0
-        pb = np.ones((3)) * 10.0
-        center,  r = sum.minimize(pa, pb)
+        self.centers = np.zeros((self.num_spheres, 3))
+        self.rs = np.zeros((self.num_spheres))
+        
+        for component in range(self.num_spheres):
+            print(component)
+            select = self.labels == component
+            faces_set = self.face_set_from_points(select)
+
+            
+            sum = Sqem()
+            sum.set_zero()
+            for f in faces_set:
+                p = self.V_deform[self.surface_F[f, 0]]
+                n = self.N[f]
+                sf = Sqem(p, n)
+                sum = sum + sf
+
+            pa = np.ones((3)) * -10.0
+            pb = np.ones((3)) * 10.0
+            center,  r = sum.minimize(pa, pb)
+
+            self.centers[component] = center
+            self.rs[component] = r
+
+        print(self.centers)
+        self.added_sphere = ps.register_point_cloud("added", self.centers.reshape((-1, 3)), radius = 0.1)
+        self.added_sphere.add_scalar_quantity("radius", self.rs)
+        self.added_sphere.set_point_radius_quantity("radius")
+        
 
 
-        self.added_sphere = ps.register_point_cloud("added", center.reshape((-1, 3)), radius = r)
+    def face_set_from_points(self, bool_selection):
+        faces_set = np.zeros((0), dtype = np.int32)
+        for _v in np.argwhere(bool_selection):
+            v = int(_v)
+            fv = self.vf[self.ni[v]: self.ni[v + 1]]
+            faces_set = np.concatenate((faces_set, fv))
 
+        return faces_set
+
+        
     def extract_max_points(self):
         if self.h is None:
             print("hausdorff not computed")
@@ -160,11 +205,8 @@ class PSViewer():
         thres= hmax * self.threshold
 
         self.points_set = self.h > thres
-        self.faces_set = np.zeros((0), dtype = np.int32)
-        for _v in np.argwhere(self.points_set):
-            v = int(_v)
-            fv = self.vf[self.ni[v]: self.ni[v + 1]]
-            self.faces_set = np.concatenate((self.faces_set, fv))
+
+        self.faces_set = self.face_set_from_points(self.points_set)
 
         
 
@@ -182,6 +224,7 @@ class PSViewer():
 
 if __name__ == "__main__":
     ps.init() 
+    ps.set_ground_plane_mode("none")
     model = "cap"
     Q = np.load(f"output/{model}/Q.npy")
     eigenvalues = np.load(f"output/{model}/eigenvalues.npy")
